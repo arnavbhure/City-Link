cc1366a073a9308117e630c550e5ed7c45364c00
arnavbhure <arnavbhure1208@gmail.com>
2026-04-18 00:33:13 +0530

    Update db.js for environment-specific database config
    Refactor database connection setup for production and development environments.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,21 @@
-const { Pool } = require("pg");
 require("dotenv").config();
+const { Pool } = require("pg");
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+const isProduction = process.env.NODE_ENV === "production";
+
+const pool = isProduction
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    })
+  : new Pool({
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+    });
 
 module.exports = pool;

3a91ca1009328eab4fc87bd291e26bea2257d4bf
Arnav Bhure <arnavbhure1208@gmail.com>
2026-04-18 00:28:09 +0530

    Modified db.js for production and development

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,21 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-const user = process.env.DB_USER;
-const host = process.env.DB_HOST;
-const database = process.env.DB_NAME;
-const password = process.env.DB_PASSWORD;
-const port = process.env.DB_PORT;
+const isProduction = process.env.NODE_ENV === "production";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+const pool = isProduction
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    })
+  : new Pool({
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+    });
 
 module.exports = pool;
